d2837ce94685184c50ff7620510d109b6f6f44cd
Enrique Artal <artal@unizar.es>
2024-02-28 20:46:36 +0100

    add class in all.py

--- src/sage/schemes/generic/all.py
+++ src/sage/schemes/generic/all.py
@@ -1,4 +1,7 @@
 # code exports
+from sage.misc.lazy_import import lazy_import
 
 from .spec import Spec
 from .hypersurface import ProjectiveHypersurface, AffineHypersurface
+
+lazy_import('sage.schemes.generic.zeta_function', 'ZetaFunctions')
